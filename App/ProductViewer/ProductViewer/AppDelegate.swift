51068fb39c6b89d1a4156989b777a638401de92e
Kevin Onken <Kevin.Onken@target.com>
2019-11-22 11:52:35 -0600

    Silence runtime warning for assigning root view to window

--- App/ProductViewer/ProductViewer/AppDelegate.swift
+++ App/ProductViewer/ProductViewer/AppDelegate.swift
@@ -18,7 +18,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = listCoordinator.viewController
-        window?.addSubview(listCoordinator.viewController.view)
         window?.makeKeyAndVisible()
         
         return true
